f7e6e3fc8c7c667a000993b88b0e19071d00d314
YoselynValest <54320991+YoselynValest@users.noreply.github.com>
2020-02-27 17:45:36 -0500

    Update RegisterAlerts.java

--- src/test/java/deepblue/RegisterAlerts.java
+++ src/test/java/deepblue/RegisterAlerts.java
@@ -5,18 +5,7 @@ import org.openqa.selenium.WebDriver;
 
 public class RegisterAlerts extends Base{
 	
-	By pageLocator = By.xpath("/html/body/app-root/app-login/section/div[2]/img");
-	By userLocator = By.xpath("/html/body/app-root/app-login/section/div[2]/div/div/form/input[1]");
-	By passLocator =By.xpath("/html/body/app-root/app-login/section/div[2]/div/div/form/input[2]");
-	By btnLoginLocator = By.xpath("/html/body/app-root/app-login/section/div[2]/div/div/form/div/button");
-	By btnAlertsLocator = By.xpath("html/body/app-root/app-home/app-header/div[1]/ul/li[2]/a");
-	By nameAlertLocator = By.xpath("/html/body/app-root/app-alerts/app-header/div[2]/div/div[1]/input");
-	By btnCreateAlertLocator = By.xpath("/html/body/app-root/app-alerts/app-header/div[2]/div/div[1]/button");
-	By selectAlertLocator = By.cssSelector("body > div > div > div.swal2-content > select");
-	By btnOkPopup = By.xpath("/html/body/div/div/div[3]/button[1]");
-	By btnYesPopup = By.xpath("/html/body/div/div/div[3]/button[1]");
-	By btnMessageOkLocator =By.xpath("/html/body/div/div/div[3]/button[1]");
-	
+	By pageLocator = By.xpath("//*[@id="comercio"]/div[2]/button");
 	
 	public RegisterAlerts(WebDriver driver) {
 		super(driver);
@@ -25,24 +14,14 @@ public class RegisterAlerts extends Base{
 	public void pruebaAlerts () throws InterruptedException {
 		
 		if (isDisplayed(pageLocator)) {
-			envioDatos("drpv",userLocator);
-			envioDatos("drpv123",passLocator);
-			click(btnLoginLocator);
+			click(pageLocator);
 			Thread.sleep(2000);
-			click(btnAlertsLocator); 
-			Thread.sleep(1000);
-			envioDatos("AlertaPrueba",nameAlertLocator);
-			click(btnCreateAlertLocator);
-			selector(selectAlertLocator);
-			click(btnOkPopup);
-			Thread.sleep(1000);
-			click(btnYesPopup);
-			click(btnMessageOkLocator);
+		
 			}else {
-		   System.out.println("No existe la p·gina");   
+		   System.out.println("No existe la p√°gina");   
     }
 	}
 	public boolean validarTest(){
-		return isDisplayed(btnMessageOkLocator);
+		return isDisplayed(pageLocator);
 	}
 }
